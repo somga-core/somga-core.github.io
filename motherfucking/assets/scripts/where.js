911e04e7ab6534bd408529d75e9f2ac3ac451621
K0RSY <pgeorg2010@gmail.com>
2025-08-17 19:07:20 +0200

    Added where tab and keywords

---
+++ motherfucking/assets/scripts/where.js
@@ -0,0 +1,22 @@
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+}
+
+function loadLink(info) {
+  var html = loadFile("assets/elements/link.html").split("~");
+
+  return html[0] + info[1] + html[1] + info[0] + html[2];
+}
+
+var links = loadFile("../links.txt").split("\n");
+
+for (let i = 0; i < links.length; i++) {
+  document.write(loadLink(links[i].split(" ")));
+}
